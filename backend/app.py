94e622820359e56acd7de07bdb6e879cc6ad36b5
sweetroll <sweetrollbandit@protonmail.com>
2021-05-28 22:20:27 -0500

    added tslib

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,9 @@ def analyze_tweet(event, _):
     tweet_url = event['body']
     http_response = {
         "statusCode": 200,
+        "headers": {
+            'Access-Control-Allow-Origin': '*',
+        },
         "body": json.dumps({"tweet": tweet_url,"elon":"true"})
     }
     return http_response
