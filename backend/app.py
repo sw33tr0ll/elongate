80437299972105566256964535e9b1c3e4b686ed
sweetroll <sweetrollbandit@protonmail.com>
2021-05-28 22:36:47 -0500

    glocked

--- backend/app.py
+++ backend/app.py
@@ -2,12 +2,22 @@ import json
 
 def analyze_tweet(event, _):
     print(event)
-    tweet_url = event['body']
-    http_response = {
-        "statusCode": 200,
-        "headers": {
-            'Access-Control-Allow-Origin': '*',
-        },
-        "body": json.dumps({"tweet": tweet_url,"elon":"true"})
-    }
-    return http_response
+    try:
+        tweet_url = event['body']
+        http_response = {
+            "statusCode": 200,
+            "headers": {
+                'Access-Control-Allow-Origin': '*',
+            },
+            "body": json.dumps({"tweet": tweet_url,"elon":"true"})
+        }
+        return http_response
+    except Exception as e:
+        print(e)
+        http_response = {
+            "statusCode": 500,
+            "headers": {
+                'Access-Control-Allow-Origin': '*',
+            },
+            "body": json.dumps({"error": e})
+        }
